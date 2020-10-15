e09600ade057cb57a5120b765b11ffb73a4ce2f3
GudlaArunKumar <arunkumarakansha@gmail.com>
2020-10-15 21:53:06 +0530

    Added BBC News headlines scrapper

---
+++ Python/BBCnews.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+response = requests.get("https://www.bbc.com/")
+
+parsed_html = BeautifulSoup(response.text,'html.parser')
+
+headlines_links = parsed_html.select('.media__link')
+
+def bbcHeadlines(headlines_links):
+    news = []
+    for links in headlines_links:
+        headlines = links.get_text('\n',strip=True)
+        if links.get('href').startswith('https'):
+            link = links.get('href')
+            news.append({'HeadLines ':headlines, 'Link': link})
+        else:
+            link = links.get('href')
+            news.append({'HeadLines ': headlines, 'Link': 'https://www.bbc.com'+link})
+
+    return news
+
+pprint.pprint(bbcHeadlines(headlines_links))
