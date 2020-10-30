b2b2d0d1f9f07b23ab02df0962667b61d31d7407
Avishek <avi.adonis241997@gmail.com>
2020-10-30 22:25:50 +0530

    Added 1029. Two City Scheduling Solution in Java

---
+++ LeetCode/1029. Two City Scheduling.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        
+        Arrays.sort(costs, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+               return o1[0] - o1[1] - (o2[0] - o2[1]);
+            }
+        });
+
+        int total = 0;
+        
+        int n = costs.length/2;
+        
+        for(int i = 0; i<n; ++i) {
+           
+            total += costs[i][0] + costs[i+n][1];
+        }
+                
+
+        return total;
+        
+    }
+}
