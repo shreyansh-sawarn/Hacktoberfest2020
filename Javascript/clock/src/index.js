8a32522525e20d740955516e376d23c160aec0a6
4rturd13 <4rturd13@gmail.com>
2020-10-31 13:23:03 -0500

    added clock javascript folder

---
+++ Javascript/clock/src/index.js
@@ -0,0 +1,24 @@
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+let day = new Date();
+let hh = day.getHours() * 30;
+let mm = day.getMinutes() * deg;
+let ss = day.getSeconds() * deg;
+
+hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+mn.style.transform = `rotateZ(${mn}deg)`;
+sc.style.transform = `rotateZ(${ss}deg)`;
+
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
