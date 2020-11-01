92944b87d9582142f518a04569878934a9fc3d56
M0RG4N01 <morgan.mcneil@live.co.uk>
2020-11-01 00:09:48 +0000

    Added fun CS questionnaire

---
+++ Python/CS_Fun_Questionare.py
@@ -0,0 +1,251 @@
+#Questionare
+#Written by Morgan McNeil
+
+# Score starts at 0
+
+score = 0           
+
+print('Welcome to the computer science multiple choice quiz!')
+
+print('Question 1')
+
+print('What does CPU stand for?')
+
+print(' 1. Central Process Unit')
+print(' 2. Center Processing Unit')
+print(' 3. Central Processing Unit')
+print(' 4. Center Process Unit')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '3':
+
+    print('Yes that is correct!')
+    score = score + 1
+
+else:
+
+    print('No that was wrong, the correct answer was 3!')
+
+print('Question 2')
+
+print('What does RAM stand for?')
+
+print(' 1. Remote Automatic Memory')
+print(' 2. Random Automatic Memory')
+print(' 3. Read Access Memory')
+print(' 4. Random Access Memory')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '4':
+
+    print('Yes that is correct!')
+    score = score + 1
+
+else:
+
+
+    print('No that was wrong, the correct answer is 4!')
+
+print('Question 3')
+
+print('What does CLI stand for?')
+
+print(' 1. Commanding Line Interface')
+print(' 2. Commander Line Interface')
+print(' 3. Command Lino Interface')
+print(' 4. Command Line Inteface')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '4':
+
+    print('Yes that is correct!')
+    score = score + 1
+
+else:
+
+
+    print('No that was wrong, the correct answer is 4!')
+
+print('Question 4')
+
+print('What is the largest number of bytes called?')
+
+print('1. Byte')
+print('2. Zettabyte')
+print('3. Yottabyte')
+print('4. Exabyte')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '3':
+
+    print('Yes that is correct!')
+    score = score + 1
+
+else:
+
+
+    print('No that was wrong, the correct answer is 3!')
+
+print('Question 5')
+
+print('What is 11000110 in denary?')
+
+print('1. 198')
+print('2. 163')
+print('3. 126')
+print('4. 1337')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '1':
+
+    print('Yes that is correct!')
+    score = score + 1
+
+else:
+
+
+    print('No that was wrong, the correct answer is 1!')
+
+print('Question 6')
+
+print('What is 00101011 in hexadecimal?')
+
+print('1. 3B')
+print('2. 2B')
+print('3. 4C')
+print('4. 69G')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '2':
+
+    print('Yes that is correct!')
+    score = score + 1
+    
+else:
+
+
+    print('No that was wrong, the correct answer is 2!')
+
+print('Question 7')
+
+print('What does BIOS stand for?')
+
+print('1. Basic Insight Outsight System')
+print('2. Blocked Input Output System')
+print('3. Basic Input Output System')
+print('4. Basic Input Override System')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '3':
+
+    print('Yes that is correct!')
+    score = score + 1
+
+else:
+
+
+    print('No that was wrong, the correct answer is 3!')
+
+print('Question 8')
+
+print('What does HDMI stand for?')
+
+print('1. High Definition Media Input')
+print('2. High Definition Medical Interface')
+print('3. High Definition Multi-media Input')
+print('4. High Definition Multi-media Interface')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '4':
+
+    print('Yes that is correct!')
+    score = score + 1
+
+else:
+
+
+    print('No that was wrong, the correct answer is 4!')
+
+
+print('Question 9')
+
+print('What does LAN stand for?')
+
+print('1. Local Airspace Network')
+print('2. Local Area Network')
+print('3. Local Access Network')
+print('4. Localized Access Network')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '2':
+
+    print('Yes that is correct!')
+    score = score + 1
+
+else:
+
+
+    print('No that was wrong, the correct answer is 2!')
+
+print('Question 10')
+
+print('What does Mbps stand for?')
+
+print('1. Megabytes per second')
+print('2. Megabit per second')
+print('3. Megabits per second')
+print('4. Mega bits per second')
+
+print('Please choose (1-4) \n:')
+
+answer = input()
+
+if answer == '3':
+
+    print('Yes that is correct! \n:')
+    score = score + 1
+
+    print('Quiz Over! You Scored', score,'!')
+
+else:
+
+
+    print('No that was wrong, the correct answer is 3! \n:')
+
+    print('Quiz Over! You Scored', score,'!')
+
+
+
+
+
+
+
+
+
